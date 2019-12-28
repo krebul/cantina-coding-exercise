f18c1e77c79bc799b392cd45a2e2c4790dc321e6
fdoughty <fdoughty@netdes.com>
2019-12-28 16:11:14 -0500

    Create project

---
+++ src/main/java/com/fdoughty/cantina/CantinaCodingExcercise.java
@@ -0,0 +1,13 @@
+package com.fdoughty.cantina;
+
+/**
+ * Hello world!
+ *
+ */
+public class CantinaCodingExcercise 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/fdoughty/cantina/CantinaCodingExcerciseTest.java
@@ -0,0 +1,38 @@
+package com.fdoughty.cantina;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple CantinaCodingExcercise.
+ */
+public class CantinaCodingExcerciseTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CantinaCodingExcerciseTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CantinaCodingExcerciseTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}

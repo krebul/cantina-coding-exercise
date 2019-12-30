b7477dc9d2b7108adb9547afeced914035b0187b
fdoughty <fdoughty@webbirdie.com>
2019-12-29 20:23:30 -0500

    perform search for single selector

--- src/main/java/com/fdoughty/cantina/CantinaCodingExcercise.java
+++ src/main/java/com/fdoughty/cantina/CantinaCodingExcercise.java
@@ -3,13 +3,18 @@ package com.fdoughty.cantina;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fdoughty.cantina.domain.SystemView;
+
+import com.fdoughty.cantina.service.selectorprocessor.Selector;
 import com.fdoughty.cantina.service.selectorprocessor.SelectorProcessorService;
 
 /**
@@ -20,37 +25,49 @@ public class CantinaCodingExcercise
 {
 	public static final String FILE_URL = "https://raw.githubusercontent.com/jdolan/quetoo/master/src/cgame/default/ui/settings/SystemViewController.json";
 	private static SelectorProcessorService svc;
+	private static final Logger logger = LoggerFactory.getLogger(CantinaCodingExcercise.class);
 	
 	
     public static void main( String[] args )
     {
     	svc = SelectorProcessorService.getInstance();
         System.out.println( FILE_URL );
+        Scanner scanner = new Scanner(System.in);
         try
 		{
-			loadInputData();
-			findElement(".Input");
+        	JsonNode rootNode = loadViewData();
+        	
+        	while (true) {
+				
+        		System.out.println("Please enter a selector.");
+				String selectorStr = scanner.next();
+				Selector selector = svc.parseSelector(selectorStr);
+				List<String> results = svc.findNodeFromView(rootNode, selector, null);
+				for (String result : results) {
+					logger.info(result);
+				}
+				logger.info("{} result(s) found.", results.size());
+        	}
+        	
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
+        scanner.close();
 		
     }
-    
-    private static List<String> findElement(String selector) throws Exception
-    {
-    	List<String> results = new ArrayList<>();
-    	svc.parseSelector(selector);
-    	return results;
-    }
+
     
     
-    private static SystemView loadInputData() throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
+    private static JsonNode loadViewData() throws JsonParseException, JsonMappingException, MalformedURLException, IOException {
     	ObjectMapper mapper = new ObjectMapper();
 
     	//JSON URL to Java object
-    	SystemView obj = mapper.readValue(new URL(FILE_URL), SystemView.class);
-    	return obj;
+    	//SystemView obj = mapper.readValue(new URL(FILE_URL), SystemView.class);
+    	JsonNode root = mapper.readTree(new URL(FILE_URL));
+    	
+    	
+    	return root;
     }
 }
--- src/main/java/com/fdoughty/cantina/service/selectorprocessor/SelectorProcessorService.java
+++ src/main/java/com/fdoughty/cantina/service/selectorprocessor/SelectorProcessorService.java
@@ -1,8 +1,15 @@
 package com.fdoughty.cantina.service.selectorprocessor;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fdoughty.cantina.service.selectorprocessor.Selector.SelectorType;
 
 
@@ -22,7 +29,10 @@ public class SelectorProcessorService
 		return singleton;
 	}
 	
-	
+	/* parseSelector
+	 * Input: String containing search selector
+	 * Output: Selector object containing data parsed from selector string
+	 */
 	public Selector parseSelector(String selectorStr) throws Exception {
 		Selector selector = new Selector();
 		logger.debug("processing selector [{}]", selectorStr);
@@ -55,5 +65,63 @@ public class SelectorProcessorService
 		logger.debug("SelectorName [{}]", selector.getSelectorName());
 		
 		return selector;
+	}
+	
+	public List<String> findNodeFromView(JsonNode rootNode, Selector selector, String path) throws Exception {
+		List<String> results = new ArrayList<>();
+		JsonNode theNode = null;
+		if (path == null)
+			path = "/";
+		
+		//logger.debug("Searching for node [{}] at [{}]", selector.getSelectorName(), path);
+		switch (selector.getType()) {
+			case CLASS:
+				if (!rootNode.path("class").isMissingNode()) {
+					if (rootNode.path("class").textValue().equalsIgnoreCase(selector.getSelectorName())) {
+						results.add(path + rootNode.path("class").textValue());
+						
+					}
+				}
+				break;
+			case CLASSNAME:
+				if (!rootNode.path("classNames").isMissingNode()) {
+					ArrayNode classArr = (ArrayNode) rootNode.path("classNames");
+					if (classArr != null) {
+						for (int index = 0; index < classArr.size(); index++)
+						{
+							if (classArr.get(index).textValue().equalsIgnoreCase(selector.getSelectorName()))
+								results.add(path + "." + classArr.get(index).textValue());
+						}
+						
+					}
+				}
+				break;
+			case IDENTIFIER:
+				if (!rootNode.path("identifier").isMissingNode()) {
+					if (rootNode.path("identifier").textValue().equalsIgnoreCase(selector.getSelectorName())) {
+						results.add(path + "#" + rootNode.path("identifier").textValue());
+						
+					}
+				}
+				break;
+			default:
+				throw new Exception("Unknown Selector Type: " + selector.getType());
+		}
+		
+		Iterator<Entry<String, JsonNode>> i = rootNode.fields();
+		while (i.hasNext()) {
+			Entry<String, JsonNode> entry = i.next();
+			JsonNode field = rootNode.get(entry.getKey());
+			if (field.isObject()) {
+				results.addAll(findNodeFromView(field, selector, path + entry.getKey() + "/"));
+			}
+			
+			else if (field.isArray()) {
+				for (JsonNode svNode : field){
+					results.addAll(findNodeFromView(svNode, selector, path + entry.getKey() + "/"));
+				}
+			}
+		}
+		return results;
 	}
 }
